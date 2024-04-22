a842ef1fd4bf253c76116e842d8937e0beec30d2
Bruna Chaves <bcbs.brunachaves@gmail.com>
2024-04-22 18:48:19 +0200

    memset, not checked/tested

---
+++ libft/ft_memset.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void    *ft_memset(void *s, int c, unsigned int n)
+{
+    unsigned int i;
+    unsigned char *p;
+
+    i = 0;
+    p = s;
+    while (i < n)
+    {
+        p[i] = c;
+        i++;
+    }
+    return (s);
+}
