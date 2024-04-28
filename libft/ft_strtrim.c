841f1ddf689ddde4f3dd9f11ca7f2f021eb3334f
Bruna Chaves <bcbs.brunachaves@gmail.com>
2024-04-28 21:37:32 +0200

    last minute ideas

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -12,47 +12,45 @@
 
 #include "libft.h"
 
-int	ft_start_index(char const *s1, char const *set)
+int	ft_isset(char c, const char *set)
 {
-	int	start_index;
-	int	i;
-	int	is_set;
-
-
-	start_index = 0;
-	is_set = 1;
-	while (*s1)
+	while (*set)
 	{
-		i = 0;
-		while (set[i] && is_set)
-		{
-			if(*s1 != set[i])
-				i++;
-			
-		}
+		if(c == *set)
+			return (1);
+		set++;
 	}
+	return(0);
+}
 
+size_t	ft_start_index(char const *s1, const char *set)
+{
+	size_t	start_index;
+	size_t i;
+
+	i = 0;
+	start_index = 0;
+	while (s1[i] && start_index == 0)
+	{
+		while(ft_isset(s1[i], set))
+			i++;
+		start_index = i;
+	}
 	return (start_index);
 }
 
-int	ft_end_index(char const *s1, char const *set)
+size_t	ft_end_index(char const *s1, char const *set)
 {
-	int	end_index;
-	int	j;
-	int	check_times;
+	size_t	end_index;
+	size_t	i;
 
-	end_index = ft_strlen(s1) - 1;
-	check_times = ft_strlen(set);
-	while (s1[end_index] && check_times)
+	i = 0;
+	end_index = 0;
+	while (s1[i] && end_index == 0)
 	{
-		j = 0;
-		while (set[j])
-		{
-			if (s1[end_index] == set[j])
-				end_index--;
-			j++;
-		}
-		check_times--; 
+		while(ft_isset(s1[i], set))
+			i--;
+		end_index = i;
 	}
 	return (end_index);
 }
@@ -60,24 +58,28 @@ int	ft_end_index(char const *s1, char const *set)
 char	*ft_strtrim(char const *s1, char const *set)
 {
 	char	*str;
-	int		start_index;
-	int		end_index;
-	int		size_newstr;
-	int		i;
+	size_t		start_index;
+	size_t		end_index;
+	size_t		size_newstr;
+	size_t		i;
 
 	start_index = ft_start_index(s1, set);
 	end_index = ft_end_index(s1, set);
+	if (start_index >= end_index)
+		return (NULL);
 	size_newstr = end_index - start_index + 1;
 	str = (char *)malloc(size_newstr * sizeof(char) + 1);
 	if (str == NULL)
 		return (NULL);
-	i = 0;
+	i = 0; 
 	while (start_index <= end_index)
 	{
 		str[i] = s1[start_index];
 		i++;
 		start_index++;
-	}
-	str[i] = '\0';
+	} 
+/* 	str = ft_memcpy(str, s1, size_newstr); */
+	str[size_newstr] = '\0';
 	return (str);
 }
+
