de515e463534e016fac98413959d2d1dfa708438
Bruna Chaves <bcbs.brunachaves@gmail.com>
2024-07-31 12:35:09 +0200

    idea for small groups, still not good enough

--- push_swap/sort.c
+++ push_swap/sort.c
@@ -12,6 +12,53 @@
 
 #include "push_swap.h"
 
+void	sort_limits(t_list **a_list, t_list **b_list)
+{
+	int size_a;
+	int num1;
+	int num2;
+	int num3;
+
+	size_a = ft_lstsize(*a_list);
+	while (size_a > 2)
+	{
+		push_b(a_list, b_list);
+		size_a = ft_lstsize(*a_list);
+	}
+	num1 = ft_atoi((*a_list)->content);
+	num2 = ft_atoi((*a_list)->next->content);
+	if (num1 > num2)
+		swap_a(*a_list);
+	while(*b_list)
+	{
+		num1 = ft_atoi((*a_list)->content);
+		num2 = ft_atoi((*a_list)->next->content);
+		num3 = ft_atoi((*b_list)->content);
+		if (num3 < num1)
+			push_a(a_list, b_list);
+		else if (num3 > num2)
+		{
+			push_a(a_list, b_list);
+			rotate_a(a_list);
+		}
+		else
+		{
+			if (num1 - num3 < num3 - num2)
+			{
+				push_a(a_list, b_list);
+				swap_a(*a_list);
+			}
+			else
+			{
+				rotate_reverse_a(a_list);
+				push_a(a_list, b_list);
+				rotate_a(a_list);
+				rotate_a(a_list);
+			}
+		}
+	}
+}
+
 int	is_list_sorted(t_list *a_list)
 {
 	int num1;
@@ -43,8 +90,10 @@ void	sort_list(t_list **a_list)
 	b_list = NULL;
 	while (i >= 0 && !(is_list_sorted(*a_list)))
 	{
+		if(size_list < 10)
+			sort_limits(a_list, &b_list);
 		j = 0;
-		while ((*a_list)->next && j < size_list && !(is_list_sorted(*a_list)))
+		while ((*a_list) && j < size_list && !(is_list_sorted(*a_list)))
 		{
 			buffer = (*a_list)->content;
 			if (buffer[i] == '0')
